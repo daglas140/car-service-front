e92d6c114d727f157583e36c6fa832c0d71ce70e
Sebastian Gawlas <sebastianhrb@gmail.com>
2022-01-29 17:38:33 +0100

    new($basic): add missing NabigateConst ()

---
+++ src/app/model/constants/NavigateUrls.ts
@@ -0,0 +1,7 @@
+export class NavigateUrls {
+  public static CALENDAR = 'calendar';
+  public static CLIENTS = 'clients';
+  public static CARS = 'cars';
+  public static LOGIN = 'login';
+  public static HOME = 'home';
+}
